b0c08e4bd9019ba46b56275d3b597a196587127d
Kanggwanju <xprtktm1@gmail.com>
2025-08-26 08:57:52 +0900

    chap01-1 변수

---
+++ chap01-1.py
@@ -0,0 +1,35 @@
+# 변수
+# 특수문자 제한, 대소문자 구별, 예약어 제한
+age = 20
+my_name = '홍길동'
+# for = 12
+AGE = 30
+
+
+friend_name = "하츄핑"
+favorite_food = '탕수육'
+
+# 이 변수는 학생인지 여부를 확인하는 변수야!
+is_student = True
+is_graduated = False # 이 변수는 졸업여부를 확인해!
+
+print('안뇽아뇽??')
+print("파이썬은 정말 쉽구나!!")
+print(age)
+
+print('===========')
+print(my_name)
+print('my_name')
+
+hobby = '공부'
+print(hobby)
+
+print('===========')
+
+print('안녕', '잘가', '메롱', is_student, '하이')
+
+my_name = '뽀로로'
+age = 5
+print('제 이름은', my_name, '이고, 나이는', age, '살입니다.')
+print('제 이름은', friend_name, '이고, 나이는', AGE, '살입니다.')
+
