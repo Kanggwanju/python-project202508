40a8999918b955e632e488a415a0f8404e4c93af
Kanggwanju <xprtktm1@gmail.com>
2025-08-28 23:40:10 +0900

    chap02-5 % 포맷팅, format(), f-string

---
+++ chap02-5.py
@@ -0,0 +1,13 @@
+
+
+my_name = '하츄핑'
+age = 5
+
+# % formatting
+# print('제 이름은 ', my_name, '이고, 나이는 ', age, '살입니다.', sep='')
+
+# print('제 이름은 %s이고, 나이는 %d살입니다.' % (my_name, age))
+# print('제 이름은 {}이고, 나이는 {}살입니다.'.format(my_name, age))
+
+# f-string
+print(f'제 이름은 {my_name}이고, 나이는 {age}살입니다.')
