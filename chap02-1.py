a71cce3a107e513203676178bb2eaddd2fe4623d
Kanggwanju <xprtktm1@gmail.com>
2025-08-28 23:22:34 +0900

    chap02-1 입력

---
+++ chap02-1.py
@@ -0,0 +1,11 @@
+
+# my_name = input('당신의 이름은 무엇입니까? : ')
+# print('입력한 이름:', my_name)
+
+# 숫자 2개를 입력받아 더하기
+num1 = int(input('첫번째 숫자를 입력하세요! - '))
+num2 = int(input('두번째 숫자를 입력하세요! - '))
+
+result = num1 + num2
+
+print('두 숫자의 합은?', result)
