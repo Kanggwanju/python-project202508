cf72281f79c10a9280eda0e0f4231ba2e7c0dba3
Kanggwanju <xprtktm1@gmail.com>
2025-09-03 23:40:38 +0900

    chap05-4 리스트 메서드 (len, sort, reverse)

---
+++ chap05-4.py
@@ -0,0 +1,20 @@
+
+numbers = [5, 2, 8, 1, 9]
+numbers.append(11);
+
+# len, 리스트 길이
+print(len(numbers))
+print(numbers)
+
+# reverse, 리스트 순서 뒤집기
+numbers.reverse()
+print(numbers)
+
+# sort, 리스트 오름차 정렬
+numbers.sort()
+print(numbers)
+
+# reverse, 리스트 순서 뒤집기
+# 오름차 정렬 후 순서 뒤집으면 내림차 정렬
+numbers.reverse()
+print(numbers)
