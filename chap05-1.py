1a59c2a45a7b0eecc7f571e79730d4c31cbbfa81
Kanggwanju <xprtktm1@gmail.com>
2025-09-03 23:01:07 +0900

    chap05-1 리스트, 인덱스

---
+++ chap05-1.py
@@ -0,0 +1,24 @@
+
+# 리스트
+le_sserafim = ['김채원', '사쿠라', '허윤진', '카즈하', '홍은채']
+
+print(le_sserafim)
+
+lotto_num = [7, 12, 24, 33, 41, 45]
+
+my_profile = [24, '하츄핑', False, 175.6]
+
+print('==========')
+
+# 0번 인덱스 출력
+print(le_sserafim[0])
+
+# 2번 인덱스 출력
+print(le_sserafim[2])
+
+leader = le_sserafim[0]
+print(f'르세라핌의 리더는 {leader}입니다.')
+print(f'르세라핌의 막내는 {le_sserafim[4]}입니다.')
+
+# 인덱스 범위 준수
+# print(f'르세라핌의 비밀멤버는 {le_sserafim[5]}입니다.')
