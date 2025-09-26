d24f0734bccb2b43ce3f068a16bfbd238bbfcb44
Kanggwanju <xprtktm1@gmail.com>
2025-09-26 17:16:25 +0900

    chap10-1 기출 풀이 1: Set

---
+++ chap10-1.py
@@ -0,0 +1,11 @@
+# 기출 풀이 1. Set, add, remove, update
+# 순서 없음, 중복 x
+
+countries = {'일본', '중국', '한국'}
+
+countries.add('베트남')
+countries.add('중국')
+countries.remove('일본')
+countries.update(['홍콩', '한국', '태국'])
+
+print(countries)
